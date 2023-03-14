680c20d4e28d84abfe2f22bfbd3f0683f7ec8df6
12345432mh <127291858+12345432mh@users.noreply.github.com>
2023-03-14 19:05:47 +0900

    gg

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const a = 5;
-const b = 2;
-const  
+const age = parseInt (prompt("How old are you?"));
 
-console.log(a+b)
-console.log(a*b)
-console.log(a/b)
+
+if(isNaN(age)){
+    console.log("Plase write a number")
+} else {
+    console.log("Thank you for writing your age")
+}
