cad2a5e0f4016d565dd969e5240bdf77090ff183
12345432mh <127291858+12345432mh@users.noreply.github.com>
2023-03-14 19:34:00 +0900

    ss

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const age = parseInt (prompt("How old are you?"));
 
 
-if(isNaN(age)){
-    console.log("Plase write a number")
-} else {
-    console.log("Thank you for writing your age")
-}
+if(isNaN(age) || age < 0){
+    console.log("Plase write a real positive number");
+} else  if(age < 18) {
+    console.log("Your age too young.");
+} else if(age >= 18 && age <= 50){
+    console.log("You can drink");
+} else if(age > 50 && age <= 80){
+    console.log("You can't drink.")
+} else if(age === 100){
+    console.log("wow");
+} else if(age > 80) {
+    console.log("You can sometimes drink");
+} 
