96c559e9c51bef40a5d0229d0ae0fe44084e17f2
12345432mh <127291858+12345432mh@users.noreply.github.com>
2023-03-21 21:26:07 +0900

    ddd

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-console.dir(h1);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
+function onLoginSubmit(event) {
+    event.preventDefault(); //기본동작 금지
+    loginForm.classList.add(HIDDEN_CLASSNAME); // 다시 숨길거야
+    const username = loginInput.value; //loginInput.value를 username에 넣기
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    //h1에 입력한 값 넣어주기. = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    //"hidden"이라는 class 제거하기
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
